9da35738dc7dac2fe314d73034cf9641e3afc5b4
akshaybembalkar <abembalkar@mail.csuchico.edu>
2016-08-23 15:10:39 -0700

    Added Problem Description

--- Ice Cream Parlor A.K.A. 2 Sum problem/IceCreamParlor.java
+++ Ice Cream Parlor A.K.A. 2 Sum problem/IceCreamParlor.java
@@ -1,3 +1,25 @@
+/*
+ * Each time Sunny and Johnny take a trip to the Ice Cream Parlor, they pool together m
+ * dollars for ice cream. On any given day, the parlor offers a line of flavors. 
+ * Each flavor, i, is numbered sequentially with a unique ID number from 1 to n and 
+ * has a cost, ci, associated with it.
+
+ * Given the value of m and the cost of each flavor for t trips to the Ice Cream Parlor, 
+ * help Sunny and Johnny choose two flavors such that they spend their entire pool of money
+ * (m) during each visit. For each trip to the parlor, print the ID numbers for the two 
+ * types of ice cream that Sunny and Johnny purchase as two space-separated integers on 
+ * a new line. You must print the smaller ID first and the larger ID second.
+
+Note: Two ice creams having unique IDs i and j may have the same cost
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
 import java.io.*;
 import java.util.*;
 
